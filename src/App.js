3d3835ea59d5bca7d761b8db7a5d9ba5001a0488
Ryan Erkal <ryanerkal@yahoo.ie>
2023-06-24 05:20:39 +0100

    initial

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+	return (
+		<>
+			<Nav />
+			<About />
+			<Portfolio />
+			<Contact />
+			<Footer />
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "../index.css";
+
+export default function Nav() {
+	function toggleDarkMode() {
+		const html = document.querySelector("html");
+		const currentClass = html.getAttribute("class");
+		if (currentClass === "dark") {
+			html.setAttribute("class", "");
+		} else {
+			html.setAttribute("class", "dark");
+		}
+		document.getElementById("dark-mode-toggle").innerHTML =
+			html.getAttribute("class") === "dark"
+				? '<span class="material-symbols-outlined">light_mode</span>'
+				: '<span class="material-symbols-outlined">dark_mode</span>';
+	}
+
+	return (
+		<div class="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl flex flex-row justify-end">
+			<h3 class="text-slate-900 dark:text-white mr-4 hover:scale-125 hover:text-red-600 transition-all">
+				About
+			</h3>
+			<h3 class="text-slate-900 dark:text-white mr-4 hover:scale-125 hover:text-red-600 transition-all">
+				Portfolio
+			</h3>
+			<h3 class="text-slate-900 dark:text-white mr-4 hover:scale-125 hover:text-red-600 transition-all">
+				Contact
+			</h3>
+			<button onClick={toggleDarkMode} id="dark-mode-toggle">
+				<span id="dark-mode-icon" class="material-symbols-outlined">
+					dark_mode
+				</span>
+			</button>
+		</div>
+	);
+}
