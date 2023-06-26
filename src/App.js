5e96bb1687ea0f7d85548d30a66431fee94ea1fd
Ryan Erkal <ryanerkal@yahoo.ie>
2023-06-26 02:20:52 +0100

    skills

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Skills from "./components/Skills";
 
 function App() {
 	return (
 		<>
 			<Nav />
 			<About />
+			<Skills />
 			<Portfolio />
 			<Contact />
 			<Footer />
