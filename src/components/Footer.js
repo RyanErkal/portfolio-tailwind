84d8d8adec8e3b4e7fd49ff8f965791861351b05
Ryan Erkal <ryanerkal@yahoo.ie>
2023-06-27 01:51:58 +0100

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,21 @@
 import React from "react";
 
 export default function Footer() {
-	return <h1>Footer</h1>;
+	return (
+		<>
+			<footer class="ralative bottom-0 text-center">
+				<p class="py-2">&copy; 2023 Ryan Erkal</p>
+				<a
+					class="px-2 py-2 hover:text-orange-400 dark:hover:text-purple-400"
+					href="https://github.com/RyanErkal">
+					GitHub
+				</a>
+				<a
+					class="px-2 py-2 hover:text-orange-400 dark:hover:text-purple-400"
+					href="https://www.linkedin.com/in/ryan-erkal-182768160/">
+					LinkedIn
+				</a>
+			</footer>
+		</>
+	);
 }
