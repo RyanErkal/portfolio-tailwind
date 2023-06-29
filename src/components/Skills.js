e6e815f0c22c78a2190ad2dd04219b413eb65f9c
Ryan Erkal <ryanerkal@yahoo.ie>
2023-06-29 23:52:05 +0100

    changes

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,11 @@
 import React from "react";
 
+//TODO fix sizing for different screen sizes
+//TODO add reusable Project cmponent
+//TODO get contact form working
+//TODO Add Stripe  Integrtion
+//TODO add Calenly Integration
+
 export default function Skills() {
 	return (
 		<>
